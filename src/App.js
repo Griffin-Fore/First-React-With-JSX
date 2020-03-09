6b2ea2112c5566602d32832114493126c1f7fdc5
Griffin-Fore <57578311+Griffin-Fore@users.noreply.github.com>
2020-03-09 16:42:56 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Hello} from './components/HelloWorldComponent'
+
+function App() {
+  return (
+    <div className="App">
+      {/* components go here */}
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorldComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Hello = props => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
